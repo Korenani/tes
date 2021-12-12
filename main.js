7a1bd422ba9e82effa83af170839bbc5cd0a214c
Andikasetyawan <88265111+Korenani@users.noreply.github.com>
2021-12-12 14:23:32 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+//Reference to DOM elements
+var kasumi = document.querySelector("#kasumi");
+const btn = document.querySelector("#btn");
+var itungan = document.querySelector("#score")
+var score = 0;
+
+//The two poping pict
+const kirakiraimg = "./bp.png";
+const dokidokiimg = "./ap.png";
+
+//The two poping sound
+const kirakirasound = new Audio("./before.mp3");
+const dokidokisound = new Audio("./after.mp3");
+
+//event handler
+addEventListener("mouseup", kirakira);
+addEventListener("mousedown", dokidoki);
+
+//event handler
+addEventListener("touchstart", function(e){
+	e.prevenDefault();
+	kirakira();
+})
+addEventListener("touchend", function(e){
+	e.prevenDefault();
+	dokidoki();
+})
+
+//the function
+function kirakira() {
+	kasumi.src = kirakiraimg;
+	kirakirasound.play();
+}
+function dokidoki() {
+	kasumi.src = dokidokiimg;
+	score++;
+	itungan.innerHTML = score;
+}
